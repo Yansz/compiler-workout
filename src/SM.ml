f0baa48810a0f561797dc9b0d7de3ce5c47b730f
Yansz <823972409@qq.com>
2019-06-03 23:58:35 +0800

    Update SM.ml

--- src/SM.ml
+++ src/SM.ml
@@ -19,25 +19,34 @@ type prg = insn list
 type config = int list * Stmt.config
 
 (* Stack machine interpreter
-
      val eval : config -> prg -> config
-
    Takes a configuration and a program, and returns a configuration as a result
 *)                         
-let rec eval conf prog = failwith "Not yet implemented"
-
+let evalInstr (stack, (state, input, output)) prg = match  prg with
+  | BINOP op -> (match stack with 
+    | y::x::tail -> ((Expr.evalOperation op x y)::tail, (state,input,output))
+    | _ -> failwith "BINOP")
+  | CONST z -> (z::stack, (state, input,output))
+  | READ -> (match input with
+    | z::tail -> (z::stack, (state,tail,output))
+    | _ -> failwith "READ")
+  | WRITE -> (match stack with
+    | z::tail -> (tail,(state,input,output @ [z]))
+    | _ -> failwith "WRITE")
+  | LD x -> ((state x)::stack, (state, input,output))
+  | ST x -> (match stack with
+    | z::tail -> (tail,(Expr.update x z state, input, output))
+    | _ -> failwith "ST")
 (* Top-level evaluation
-
      val run : prg -> int list -> int list
-
    Takes a program, an input stream, and returns an output stream this program calculates
 *)
+let eval config prg = List.fold_left evalInstr config prg
+
 let run p i = let (_, (_, _, o)) = eval ([], (Expr.empty, i, [])) p in o
 
 (* Stack machine compiler
-
      val compile : Language.Stmt.t -> prg
-
    Takes a program in the source language and returns an equivalent program for the
    stack machine
 *)
